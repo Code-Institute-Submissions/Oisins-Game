9bb6def93297dd291a6ac9bbbdb068a062fd6d22
David Caffrey <dave.caffrey71@gmail.com>
2020-01-20 16:52:24 +0000

    added some player functionality

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -4,7 +4,7 @@ var cols = 9;
 
  function genSudArray(sudokuArray,rows,cols){
      for(var i = 0;i < rows;i++){
-            sudokuArray.push[0];
+            
          for(var j =0;j < cols;j++){
             var rand = Math.floor(Math.random()* 9 +1);
             sudokuArray[i][j] = rand;
